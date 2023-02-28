851fd1b4b035b9b960ec5e1d5f8bc506f0834309
lacctrDress20 <1490244@senati.pe>
2023-02-28 08:28:05 -0500

    configuracion de mi ventana

--- trabajo.py
+++ trabajo.py
@@ -1,5 +1,7 @@
 #Interfaz Gráfica con Tkinter para cumplir con las exigencias del usuario
 from tkinter import *
 raiz=Tk()
+miFrame=Frame(raiz, width="700", height="400")
+miFrame.pack()
 
 raiz.mainloop()
