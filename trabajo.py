abd4dc6f6454ac62c2db98ad0cbaf0ad70a28131
lacctrDress20 <1490244@senati.pe>
2023-03-01 17:32:06 -0500

    solucion para los cuadros de entrada

--- trabajo.py
+++ trabajo.py
@@ -7,21 +7,21 @@ miFrame=Frame(raiz, width="600", height="400")
 miFrame.config(bg="snow2")
 miFrame.pack()
 
-cuadroTexto=Label(miFrame, text="Ingrese sus datos").grid(row=0, column=2, padx=10, pady=10)
+cuadroTexto=Label(miFrame, text="Ingrese sus datos", font=("Baguet Script", 18)).grid(row=0, column=2, padx=10, pady=10)
 
-cuadroDNI=Entry(miFrame).grid(row=1, column=1, padx=10, pady=10)
+cuadroDNI=Entry(miFrame).grid(row=1, column=1, sticky='e', padx=10, pady=10)
 DNILabel=Label(miFrame, text="DNI:").grid(row=1, column=0, padx=10, pady=10)
 
-cuadroNombre=Entry(miFrame).grid(row=2, column=1, padx=10, pady=10)
+cuadroNombre=Entry(miFrame).grid(row=2, column=1, sticky='e', padx=10, pady=10)
 nombreLabel=Label(miFrame, text="Nombre:").grid(row=2, column=0, padx=10, pady=10)
 
-cuadroApellido=Entry(miFrame).grid(row=2, column=3, padx=10, pady=10)
+cuadroApellido=Entry(miFrame).grid(row=2, column=3, sticky='e', padx=10, pady=10)
 ApellidoLabel=Label(miFrame, text="Apellido:").grid(row=2, column=2, padx=10, pady=10)
 
-cuadroDireccion=Entry(miFrame).grid(row=3, column=1, padx=10, pady=10)
+cuadroDireccion=Entry(miFrame).grid(row=3, column=1, columnspan=3, sticky='we', padx=10, pady=10)
 DireccLabel=Label(miFrame, text="Dirección:").grid(row=3, column=0, padx=10, pady=10)
 
-cuadroTelefono=Entry(miFrame).grid(row=4, column=1, padx=10, pady=10)
+cuadroTelefono=Entry(miFrame).grid(row=4, column=1, columnspan=3, sticky='we', padx=10, pady=10)
 TelefLabel=Label(miFrame, text="Telefono:").grid(row=4, column=0, padx=10, pady=10)
 
 CodigProd=Entry(miFrame).grid(row=7, column=0, padx=10, pady=10)
@@ -39,9 +39,9 @@ Cantidad=Label(miFrame, text="Cantidad").grid(row=5, column=3, padx=10, pady=10)
 
 Precio=Label(miFrame, text="Precio").grid(row=5, column=4, padx=10, pady=10)
 
-Subtotal=Label(miFrame, text="Subtotal").grid(row=5, column=5, padx=10, pady=10)
+Subtotal=Label(miFrame, text="Subtotal").grid(row=5, column=6, padx=10, pady=10)
 
 BotonEnvio=Button(miFrame, text="total")
-BotonEnvio.grid(row=9, column=6, padx=10, pady=10)
+BotonEnvio.grid(row=10, column=7, padx=10, pady=10)
 
 raiz.mainloop()
