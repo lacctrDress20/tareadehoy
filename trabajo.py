41a03b4a14f715079229408d7d1cb5552de3778c
lacctrDress20 <1490244@senati.pe>
2023-03-01 16:51:50 -0500

    agrupacion y cuadros para el cod-prod

--- trabajo.py
+++ trabajo.py
@@ -6,27 +6,31 @@ miFrame.pack()
 
 cuadrolabel=Label(miFrame, text="Ferreteria el coloso").grid(row=0, column=2, padx=10, pady=10)
 cuadroDNI=Entry(miFrame).grid(row=1, column=1, padx=10, pady=10)
-cuadroNombre=Entry(miFrame).grid(row=2, column=1, padx=10, pady=10)
-cuadroApellido=Entry(miFrame).grid(row=2, column=3, padx=10, pady=10)
-cuadroDireccion=Entry(miFrame).grid(row=3, column=1, padx=10, pady=10)
-cuadroTelefono=Entry(miFrame).grid(row=4, column=1, padx=10, pady=10)
-
 DNILabel=Label(miFrame, text="DNI:").grid(row=1, column=0, padx=10, pady=10)
+
+cuadroNombre=Entry(miFrame).grid(row=2, column=1, padx=10, pady=10)
 nombreLabel=Label(miFrame, text="Nombre:").grid(row=2, column=0, padx=10, pady=10)
+
+cuadroApellido=Entry(miFrame).grid(row=2, column=3, padx=10, pady=10)
 ApellidoLabel=Label(miFrame, text="Apellido:").grid(row=2, column=2, padx=10, pady=10)
+
+cuadroDireccion=Entry(miFrame).grid(row=3, column=1, padx=10, pady=10)
 DireccLabel=Label(miFrame, text="Dirección:").grid(row=3, column=0, padx=10, pady=10)
+
+cuadroTelefono=Entry(miFrame).grid(row=4, column=1, padx=10, pady=10)
 TelefLabel=Label(miFrame, text="Telefono:").grid(row=4, column=0, padx=10, pady=10)
+
+CodigProd=Entry(miFrame).grid(row=7, column=0, padx=10, pady=10)
+CodigProd=Entry(miFrame).grid(row=8, column=0, padx=10, pady=10)
+CodigProd=Entry(miFrame).grid(row=9, column=0, padx=10, pady=10)
 CodigProd=Label(miFrame, text="Cod_Prod").grid(row=5, column=0, padx=10, pady=10)
+
 Descripcion=Label(miFrame, text="Descripción").grid(row=5, column=1, padx=10, pady=10)
 Unidad=Label(miFrame, text="Unidad").grid(row=5, column=2, padx=10, pady=10)
 Cantidad=Label(miFrame, text="Cantidad").grid(row=5, column=3, padx=10, pady=10)
 Precio=Label(miFrame, text="Precio").grid(row=5, column=4, padx=10, pady=10)
 Subtotal=Label(miFrame, text="Subtotal").grid(row=5, column=5, padx=10, pady=10)
 
-
-
-
 BotonEnvio=Button(raiz, text="total").pack()
-BotonEnvio.grid(row=6, column=7, padx=10, pady=10)
 
 raiz.mainloop()
