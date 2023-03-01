c2b1bd371433fce7ca8174d62fdc9196101b18c0
lacctrDress20 <1490244@senati.pe>
2023-03-01 15:39:32 -0500

    Agrego direccion y telefono

--- trabajo.py
+++ trabajo.py
@@ -1,24 +1,37 @@
 #Interfaz Gráfica con Tkinter para cumplir con las exigencias del usuario
 from tkinter import *
-raiz=Tk()
-raiz.config(bg="PaleTurquoise1")
+raiz='hola'
 miFrame=Frame(raiz, width="600", height="400")
 miFrame.pack()
-miFrame.config(bg="PaleTurquoise1")
+#------------------------------------
 cuadroDNI=Entry(miFrame)
 cuadroDNI.grid(row=0, column=1, padx=10, pady=10)
-cuadroDNI.config(bg="red")
+#------------------------------------
 cuadroNombre=Entry(miFrame)
 cuadroNombre.grid(row=1, column=1, padx=10, pady=10)
+#------------------------------------
 cuadroApellido=Entry(miFrame)
-cuadroApellido.grid(row=2, column=1, padx=10, pady=10)
+cuadroApellido.grid(row=1, column=3, padx=10, pady=10)
+
+cuadroDireccion=Entry(miFrame)
+cuadroDireccion.grid(row=2, column=1, padx=10, pady=10)
+
+cuadroTelefono=Entry(miFrame)
+cuadroTelefono.grid(row=2, column=3, padx=10, pady=10)
 
 
 DNILabel=Label(miFrame, text="DNI:")
 DNILabel.grid(row=0, column=0, padx=10, pady=10)
+#----------------------------
 nombreLabel=Label(miFrame, text="Nombre:")
 nombreLabel.grid(row=1, column=0, padx=10, pady=10)
+#-----------------------------
 ApellidoLabel=Label(miFrame, text="Apellido:")
-ApellidoLabel.grid(row=2, column=0, padx=10, pady=10)
+ApellidoLabel.grid(row=1, column=2, padx=10, pady=10)
 
+DireccLabel=Label(miFrame, text="Dirección:")
+DireccLabel.grid(row=2, column=0, padx=10, pady=10)
+
+TelefLabel=Label(miFrame, text="Telefono:")
+TelefLabel.grid(row=2, column=2, padx=10, pady=10)
 raiz.mainloop()
