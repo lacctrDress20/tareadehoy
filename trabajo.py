cdc0a5a6c1261a9ee4f723cd1175e2309d1f8bac
lacctrDress20 <1490244@senati.pe>
2023-03-01 16:09:49 -0500

    reduccion de lineas

--- trabajo.py
+++ trabajo.py
@@ -3,39 +3,20 @@ from tkinter import *
 raiz=Tk()
 miFrame=Frame(raiz, width="600", height="400")
 miFrame.pack()
-#------------------------------------
-cuadroDNI=Entry(miFrame)
-cuadroDNI.grid(row=0, column=1, padx=10, pady=10)
-#------------------------------------
-cuadroNombre=Entry(miFrame)
-cuadroNombre.grid(row=1, column=1, padx=10, pady=10)
-#------------------------------------
-cuadroApellido=Entry(miFrame)
-cuadroApellido.grid(row=1, column=3, padx=10, pady=10)
 
-cuadroDireccion=Entry(miFrame)
-cuadroDireccion.grid(row=2, column=1, padx=10, pady=10)
+cuadrolabel=Label(miFrame, text="Ferreteria el coloso").grid(row=0, column=2, padx=10, pady=10)
+cuadroDNI=Entry(miFrame).grid(row=1, column=1, padx=10, pady=10)
+cuadroNombre=Entry(miFrame).grid(row=2, column=1, padx=10, pady=10)
+cuadroApellido=Entry(miFrame).grid(row=2, column=3, padx=10, pady=10)
+cuadroDireccion=Entry(miFrame).grid(row=3, column=1, padx=10, pady=10)
+cuadroTelefono=Entry(miFrame).grid(row=4, column=1, padx=10, pady=10)
 
-cuadroTelefono=Entry(miFrame)
-cuadroTelefono.grid(row=2, column=3, padx=10, pady=10)
+DNILabel=Label(miFrame, text="DNI:").grid(row=1, column=0, padx=10, pady=10)
+nombreLabel=Label(miFrame, text="Nombre:").grid(row=2, column=0, padx=10, pady=10)
+ApellidoLabel=Label(miFrame, text="Apellido:").grid(row=2, column=2, padx=10, pady=10)
+DireccLabel=Label(miFrame, text="Dirección:").grid(row=3, column=0, padx=10, pady=10)
+TelefLabel=Label(miFrame, text="Telefono:").grid(row=4, column=0, padx=10, pady=10)
+BotonEnvio=Button(raiz, text="total").pack()
 
-
-DNILabel=Label(miFrame, text="DNI:")
-DNILabel.grid(row=0, column=0, padx=10, pady=10)
-#----------------------------
-nombreLabel=Label(miFrame, text="Nombre:")
-nombreLabel.grid(row=1, column=0, padx=10, pady=10)
-#-----------------------------
-ApellidoLabel=Label(miFrame, text="Apellido:")
-ApellidoLabel.grid(row=1, column=2, padx=10, pady=10)
-
-DireccLabel=Label(miFrame, text="Dirección:")
-DireccLabel.grid(row=2, column=0, padx=10, pady=10)
-
-TelefLabel=Label(miFrame, text="Telefono:")
-TelefLabel.grid(row=2, column=2, padx=10, pady=10)
-
-BotonEnvio=Button(raiz, text="total")
-BotonEnvio.pack()
 
 raiz.mainloop()
